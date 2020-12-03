83ded75a24e1f0c0e528145fd69828fc8696725d
Jordan <jcr.salazar@outlook.com>
2020-12-03 09:35:09 -0500

    Se optimiza la llamada HTTP

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,8 @@
-
 export const getGifs = async(category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=lsTQEvKWuHbBiByRI6bXpKpD0RJ5epWv`;
+
+    const API_KEY = 'lsTQEvKWuHbBiByRI6bXpKpD0RJ5epWv';
+    
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=${API_KEY}`;
 
     const resp = await fetch(url);
     
