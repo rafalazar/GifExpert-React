100c3dbca5359ef42af559010bc0d217ff4b196d
Jordan <jcr.salazar@outlook.com>
2020-12-09 19:00:15 -0500

    Tests sobre GifGridItem completos

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,11 +3,36 @@ import { shallow } from 'enzyme';
 import GifGridItem from '../../components/GifGridItem';
 
 describe('Pruebas en <GifGridItem />', () => {
+
+    const title = 'Soy un título';
+    const url = 'https://localhost/something.jpg';
+    const wrapper = shallow(<GifGridItem title={title} url={url} />);
+
     test('Debe mostrat el componente correctamente', () => {
 
-        const wrapper = shallow(<GifGridItem />);
-
         expect(wrapper).toMatchSnapshot();
+
     });
 
+    test('Debe tener párrafo con el title', () => {
+
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(title);
+        
+    });
+
+    test('Debe tener src y alt igual que los props', () => {
+        const img = wrapper.find('img');
+        const {src, alt} = img.props();
+        expect(src).toBe(url);
+        expect(alt).toBe(title);
+    });
+    
+    test('Debe tener la clase animate__fadeIn', () => {
+        const div = wrapper.find('div');
+        const { className } = div.props();
+        
+        expect(className.includes('animate__fadeIn')).toBe(true);
+    });
+    
 });
